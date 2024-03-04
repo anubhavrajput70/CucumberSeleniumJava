d75a9e8ff48dd6ec940aa92c406e4ea7c22a56b6
anubhavrajput70 <anubhavrajput193@gmail.com>
2024-03-04 23:58:33 +0530

    Update GoogleSearchSteps.java

--- src/test/java/StepDefinitions/GoogleSearchSteps.java
+++ src/test/java/StepDefinitions/GoogleSearchSteps.java
@@ -1,8 +1,12 @@
 package StepDefinitions;
+import static org.junit.Assert.assertThat;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.en.And;
@@ -28,18 +32,22 @@ public class GoogleSearchSteps {
 	}
 
 	@When("user enters a text in search box")
-	public void user_enters_a_text_in_search_box() {
+	public void user_enters_a_text_in_search_box() throws InterruptedException {
 	    driver.findElement(By.xpath("//textarea[@title='Search']")).sendKeys("Automation");
+	    Thread.sleep(2000);
 	}
 
 	@And("hits enter")
 	public void hits_enter() {
-	
+	    driver.findElement(By.xpath("//textarea[@title='Search']")).sendKeys(Keys.ENTER);	
 	}
 
 	@Then("user is navigated to search results")
 	public void user_is_navigated_to_search_results() {
-	    driver.close();
+		driver.getPageSource().contains("Automation is the use of technology");
+		driver.close();
+		driver.quit();
+	  
 	}
 
 
