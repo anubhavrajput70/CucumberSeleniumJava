3e73d696178dd6ae5d3418dad216da9f77e78cd8
anubhavrajput70 <anubhavrajput193@gmail.com>
2024-12-08 23:50:21 +0530

    runner update

--- src/test/java/FeatureRunner/Runner.java
+++ src/test/java/FeatureRunner/Runner.java
@@ -1,6 +1,7 @@
 package FeatureRunner;
 //we can also create the multiple runner classes for the targeted features files for different folders
 //we can run any number of runner classes with the help of the testng xml file
+//surefire is a plugin which gives us the capability to run the project through command line
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.CucumberOptions;
@@ -10,7 +11,7 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(features="src/test/resources/Features",glue= {"StepDefinitions"},
 
 		monochrome=true,
-		plugin={"pretty","html:target/HtmlReport"},
+		plugin={"pretty","html:target/HtmlReport","json:target/JsonReport.json","com.cucumber.listener.ExtentCucumberFormatter:target/report.html"},
 		tags="@login" 
 		)
 
