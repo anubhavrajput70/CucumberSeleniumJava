a8948eabbf5a12310d65f12f98a291d6b3b9a3dd
anubhavrajput70 <anubhavrajput193@gmail.com>
2024-12-16 00:40:08 +0530

    locators

--- src/test/java/pageObjects/Products_Page.java
+++ src/test/java/pageObjects/Products_Page.java
@@ -2,7 +2,15 @@ package pageObjects;
 
 import java.io.IOException;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Products_Page extends BasePage{
+	
+	public @FindBy(xpath ="//div[@id='container-special-offers']") WebElement button_specialOffers;
+	public @FindBy(xpath="//div[@id='container-product2']']") WebElement button_laptops;
+	public @FindBy(xpath="//div[@id='myModal']//b[contains(text(),'NEWCUSTOME')]") WebElement voucherNumber;
+	public @FindBy(xpath="//button[text()='Proceed']") WebElement button_proceed;
 
 	public Products_Page() throws IOException
 	{
